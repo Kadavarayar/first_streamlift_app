1d09de4958b1d9db20407d1efefa2559abb1a721
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-25 10:45:05 -0500

    Update streamlit_app.py
    Added list of items for breakfast.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My parents new health dinner')
 
 streamlit.header('Breakfast Menu')
+
+Egg Omeletter with Mushroom and Bacon
+Oatmeal with blu-berry
+Banana Muffins

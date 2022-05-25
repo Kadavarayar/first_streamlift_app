fb1f6a85222690c4354dfa3c4519a74ae78d3630
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-25 10:50:16 -0500

    Update streamlit_app.py
    Typo.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My parents new health dinner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Egg Omelette with Mushroom and Bacon')
-streamlit.text(''Oatmeal with blu-berry')
-streamlit.text(''Banana Muffins')
+streamlit.text('Oatmeal with blu-berry')
+streamlit.text('Banana Muffins')

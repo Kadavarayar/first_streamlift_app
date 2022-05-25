4c0a3d366686a4540806866664b6c7f08000b097
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-25 10:45:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My parents new health dinner')
 
 streamlit.header('Breakfast Menu')
 
-Egg Omeletter with Mushroom and Bacon
-Oatmeal with blu-berry
-Banana Muffins
+'Egg Omeletter with Mushroom and Bacon'
+'Oatmeal with blu-berry'
+'Banana Muffins'

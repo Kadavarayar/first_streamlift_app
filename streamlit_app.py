74445622b14e7c096383ba8fbe9c22606ad7ea6e
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 15:02:32 -0500

    Update streamlit_app.py
    Variable option added for fruit name.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice !')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just displays the data at row level
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json()) # just displays the data at row level
 
 # Lets normalize the json version of the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

c68d8cd4261c3de6ca396a617735fc2005824dc0
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 14:01:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.details('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 import pandas
 

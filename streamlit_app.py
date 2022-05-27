a2f4e8299d5e9277c43a091edc5c3375f54445b1
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 13:29:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Favarites')
 
+import pandas
 
-
-
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')

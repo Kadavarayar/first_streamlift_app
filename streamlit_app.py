eba983827beb17c37047b91bffec5814c81b7f76
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 13:37:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,5 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")

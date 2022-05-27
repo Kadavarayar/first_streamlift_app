718fa6fb4181bd13578366ec416ae142f59db131
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 14:25:33 -0500

    Create streamlit_app.py
    Recreated the same file for practice.

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit
+
+streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast Favarites')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard boiled Free-range Eggs')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

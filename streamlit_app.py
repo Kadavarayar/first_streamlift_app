88f5c245bde84779fdb3331daa6160acbd4812c6
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 13:25:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,3 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put multiselection option for customer to choose the fruits they want to include
-# streamlit.multiselect("Please select your fruits: ",list(my_fruit_list.index))
-# streamlit.multiselect("Please select your fruits: ",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_selected = streamlit.multiselect("Please select your fruits: ",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_to_show)

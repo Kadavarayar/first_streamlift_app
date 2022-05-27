5e33292237da5e7b29709b9625c94fe3518f1343
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 13:29:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,7 @@ streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Favarites')
 
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
-streamlit.text('🐔 Hard boiled Free-range Eggs')
-streamlit.text('🥑🍞 Avacado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 

86a3e1ae026b4ff73d6f1f23e8e553c1bebd2ef1
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-27 14:20:39 -0500

    Update streamlit_app.py
    Menu updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text("hello")
-
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard boiled Free-range Eggs')
+streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 

874cf047a3c714a1691e2fadeae646e19f6afa3c
Kadavarayar <106256191+Kadavarayar@users.noreply.github.com>
2022-05-31 11:24:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice !')
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about? ', 'kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
   streamlit.error ("Please select a fruit to get a information.")
 else: 
@@ -56,7 +56,7 @@ my_data_row = my_cur.fetchall()
 streamlit.header("My Fruit Load List Contains: ")
 streamlit.dataframe(my_data_row)
 
-add_new_fruit = streamlit.text_input('What fruit would you like to add? ', '')
+add_new_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write ('Thanks for adding ',add_new_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from Streamlit')")
